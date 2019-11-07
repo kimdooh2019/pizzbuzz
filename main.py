9e6e7c9319b6b21aeae883ca3b9fc45bdab64708
kimdooh2019 <dopa213@naver.com>
2019-11-07 16:28:35 +0900

    compelete

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 for i in range(1, 100+1):
-    print(i)
+    if i % 15 == 0:
+        print(f'pizzbuzz')
+    elif i % 3 == 0:
+        print(f'pizz')
+    elif i % 5 == 0:
+        print(f'buzz')
+    else:
+        print(i)
+
